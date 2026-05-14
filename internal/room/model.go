6daf8dfdde348f7b5c791694f51889b9d4e2c0bc
Vladyslav Fedirko <vladteua@gmail.com>
2026-05-14 22:02:06 +0300

    feat: wire room postgres repository

---
+++ internal/room/model.go
@@ -0,0 +1,29 @@
+package room
+
+type RoomStatus string
+
+const (
+	RoomStatusAvailable RoomStatus = "available"
+	RoomStatusReserved  RoomStatus = "reserved"
+)
+
+type Room struct {
+	ID         string
+	RoomNumber string
+	Status     RoomStatus
+	Capacity   int
+}
+
+type CreateRoomRequest struct {
+	ID         string `json:"id"`
+	RoomNumber string `json:"room_number"`
+	Capacity   int    `json:"capacity"`
+}
+
+type GetRoomsRequest struct {
+	Status RoomStatus `json:"status"`
+}
+
+type GetRoomByIDRequest struct {
+	ID string `json:"id"`
+}
